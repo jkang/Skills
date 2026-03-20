8a07b1558023c07fc92658bc4383f0d2f214abcb
KK <jkang@thoughtworks.com>
2026-03-20 12:45:06 +0800

    remove workPPT

--- myskills/WorkSkills/InspirePPTSkills/tests/test_import.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('/Users/superkkk/MyCoding/anthropic-skills/skills/pptx/scripts')
-import add_slide
-import clean
-import office.unpack as unpack
-import office.pack as pack
-print("Successfully imported global PPTX scripts!")
